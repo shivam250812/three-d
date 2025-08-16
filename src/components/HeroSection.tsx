5be8c875fa81633eccc41d1966be0e682cbd8415
shivam250812 <156193071+shivam250812@users.noreply.github.com>
2025-08-17 05:20:44 +0530

    Update HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -84,7 +84,7 @@ import { Canvas } from '@react-three/fiber';
 import { Environment, useGLTF, OrbitControls } from '@react-three/drei';
 import { Suspense, useEffect } from 'react';
 import * as THREE from 'three';
-import { easeOut, motion } from 'framer-motion';
+import { motion } from 'framer-motion';
 
 const Model = () => {
   const { scene } = useGLTF('/models/model.glb');
@@ -127,8 +127,8 @@ const HeroSection = () => {
 
       <div className="absolute bottom-8 left-1/2 -translate-x-1/2 z-10 w-full px-4 flex flex-col items-center">
         <p className="max-w-lg mx-auto text-gray-600 text-[17px]">
-          Upload your CAD file, and we'll take care of machining, finishing, and shipping—accurate parts delivered fast, no stress.
-        </p>
+  Upload your CAD file, and we&apos;ll take care of machining, finishing, and shipping—accurate parts delivered fast, no stress.
+</p>
         <button className="mt-7 px-7 py-3.5 bg-blue-500 text-white font-semibold rounded-md hover:bg-blue-600 shadow-lg flex items-center gap-3 mx-auto text-sm">
           <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round">
             <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
