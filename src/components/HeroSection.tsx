a2123ba1cfd9fa53a60df7d69d4ad296039aac8d
shivam250812 <156193071+shivam250812@users.noreply.github.com>
2025-08-17 00:23:42 +0530

    Update HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -7,7 +7,7 @@ import * as THREE from 'three';
 import { motion } from 'framer-motion';
 
 const Model = () => {
-  const { scene } = useGLTF('/models/forest.exr');
+  const { scene } = useGLTF('/models/texture.exr');
 
   useEffect(() => {
     scene.traverse((child) => {
@@ -66,6 +66,6 @@ const HeroSection = () => {
   );
 };
 
-useGLTF.preload('/models/landing_page_motor.glb');
+useGLTF.preload('/models/model.glb');
 
 export default HeroSection;
